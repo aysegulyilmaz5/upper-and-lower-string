e60dac61a05ca8c3cf686d3a93b7e896211f46cc
aysegulyilmaz5 <yilmazansr@gmail.com>
2021-07-06 01:20:11 +0300

    Create upperandlower.py

---
+++ upperandlower.py
@@ -0,0 +1,16 @@
+firstString = "Python is awesome"
+secondString = "PyTHon is AvESoMe"
+
+thirdString = "PYTHON IS PROGRAMMING LANGUAGE"
+fourthString = "PyThOn IS ProgrAMMINg LanGuAGE"
+
+if(secondString.upper() == firstString.upper()):
+    print("The Strings are same.")
+else:
+    print ("The string are not same")
+    
+if(thirdString.lower() == fourthString.lower()):
+    print("The Strings are same")
+else:
+    print("The Strings are not same")
+    
